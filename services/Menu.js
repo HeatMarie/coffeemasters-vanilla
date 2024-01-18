b2b2f5f2162a2cca11a52bc1d93e0a37dfc96680
Heather Bjoin <h.m.bjoin@gmail.com>
2024-01-18 12:09:40 -0600

    updating

--- services/Menu.js
+++ services/Menu.js
@@ -3,3 +3,18 @@ import API from "./API.js";
 export async function loadData() {
     app.store.menu = await API.fetchMenu()
 }
+
+export async function getProductById(id) {
+    if(app.store.menu==null) {
+        await loadData();
+    }
+
+    for (let c of app.store.menu) {
+        for (let product of c.products) {
+            if(product.id == id) {
+                return product;
+            }
+        }
+    }
+}
+
--- services/Router.js
+++ services/Router.js
@@ -26,17 +26,18 @@ const Router = {
         switch(route) {
             case "/":
                 
-                pageElement = document.createElement("h1");
+                pageElement = document.createElement("menu-page");
+                
                 pageElement.textContent = "Menu"
                 break;
             case "/order":
                 
-                pageElement = document.createElement("h1");
+                pageElement = document.createElement("order-page");
                 pageElement.textContent = "Order"
                 break;
             default: 
                 if(route.startsWith("/product-")) {
-                    pageElement = document.createElement("h1");
+                    pageElement = document.createElement("details-page");
                     pageElement.textContent = "Details";
                     const paramId = route.substring(route.lastIndexOf("-")+1);
                     pageElement.dataset.id = paramId;
